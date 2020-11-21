8914a0b207fb7bd782351399fbef9b4a64062083
puneeth4177 <71485061+puneeth4177@users.noreply.github.com>
2020-11-21 19:40:40 +0530

    Add files via upload

---
+++ Demo Package/Demo.java
@@ -0,0 +1,7 @@
+import p1.*;
+class Demo{
+public static void main(String args[]){
+Main2 m=new Main2();
+m.display();
+}
+}
---
+++ Demo Package/p1/Main2.java
@@ -0,0 +1,7 @@
+package p1;
+public class Main2{
+public int a=4;
+public void display(){
+System.out.println("result"+a);
+}
+}
